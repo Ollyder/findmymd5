d869d557572f8d807c8b506797f4779252a7c2fb
Your Name <you@example.com>
2017-08-02 07:50:37 +0800

    complete dmd5

--- findmymd5.py
+++ findmymd5.py
@@ -3,9 +3,9 @@
 try:
     import requests
     from bs4 import BeautifulSoup
-    from re import search, findall
     import re
     import hashlib
+    from random import randint
 except ImportError as e:
     print("缺少必要库")
 
@@ -67,7 +67,7 @@ class HASHTOOLKIT():
 
 
 class CMD5():
-
+    ''' http://www.cmd5.org '''
     name = 'cmd5'
     url = 'http://www.cmd5.org/'
 
@@ -87,31 +87,55 @@ class CMD5():
                 'ctl00$ContentPlaceHolder1$Button1': '查询',
                 'ctl00$ContentPlaceHolder1$HiddenField1': '',
                 'ctl00$ContentPlaceHolder1$HiddenField': 'xAo8jpr6Q7/lr81tr3uNyRcQqKX3xoyvWLbBPPK16nGyfGp7bn6977O3e1EuEoDo'}
+        # 若没有headers 则没有返回的数据
         hearders = {
-        'Host': 'www.cmd5.com',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': '1942',
-        'Referer': 'http://www.cmd5.com/',
-        'Cookie': 'ASP.NET_SessionId=eur50sa5zfbom53gff4i4ed1; comefrom=https://www.baidu.com/link?url=6sjybewmhlwquvfs2iqzvkrsuvw2dp-xgekrasr1luq&wd=&eqid=910a0fef00004180000000035980769b; FirstVisit=2017/8/1 14:41:08; Hm_lvt_0b7ba6c81309fff7ce4498ec7b107c0b=1501591271,1501592417; Hm_lpvt_0b7ba6c81309fff7ce4498ec7b107c0b=1501592417',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache'}
-
+            'Host': 'www.cmd5.com',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '1942',
+            'Referer': 'http://www.cmd5.com/',
+            'Cookie': 'ASP.NET_SessionId=eur50sa5zfbom53gff4i4ed1; comefrom=https://www.baidu.com/link?url=6sjybewmhlwquvfs2iqzvkrsuvw2dp-xgekrasr1luq&wd=&eqid=910a0fef00004180000000035980769b; FirstVisit=2017/8/1 14:41:08; Hm_lvt_0b7ba6c81309fff7ce4498ec7b107c0b=1501591271,1501592417; Hm_lpvt_0b7ba6c81309fff7ce4498ec7b107c0b=1501592417',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache'}
 
         response = s.post('http://www.cmd5.com/', data=post, headers=hearders)
         response.encoding = response.apparent_encoding
-        f = open('log.html', 'w')
-        f.write(response.text)
-        f.close()
         strings = '''<span id="ctl00_ContentPlaceHolder1_LabelAnswer">(.*)<br /><br /><a target'''
         pattern = re.compile(strings)
         m = pattern.findall(response.text)
         return m[0]
+
+
+class DMD5():
+    ''' http://www.dmd5.com/md5-decrypter.jsp'''
+
+    name = "dmd5"
+    url = "http://www.dmd5.com"
+
+    def __init__(self, hashValue):
+        self.hashValue = hashValue
+
+    def crack(self):
+        post = {'_VIEWRESOURSE' : 'c4c92e61011684fc23405bfd5ebc2b31',
+                'result' : 'c1fa08ee052e00e5b8e7527f9211d9c0453bc6f335a6181f2f58c7816f79278e75b690137e34374df3cfcf7dadfe7828dcd0c37a5bc4b493a94730b376b54c80ad8db7d2b168fff553584bff499fd06bd9dd0dbef033481c98020ec237c8c5ed54490cd46b03610d2ea2d1428e0f715ea4a79c07f40416c2de6d657f2eb6ee562d7743db5311a04aa4fa1cb0d034b6868d9abf6ec279'}
+        post['md5'] = self.hashValue
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'}
+        response = requests.post(
+            'http://www.dmd5.com/md5-decrypter.jsp', data=post, headers=headers)
+        response.encoding = response.apparent_encoding
+        html = response.text
+        with open('log.html','w') as f:
+            f.write(html)
+        string = '''解密结果：([0-9a-f]*)</p><p>密文类型：md5</p>'''
+        pattern = re.compile(string)
+        matcher = pattern.findall(html)
+        return matcher[0]
 
 
 def is_success(answer):
@@ -132,12 +156,13 @@ def is_success(answer):
 
 
 def main():
-    # TODO 最后一旦找到直接退出
-    ALLCRACK = [HASHCRACK, HASHTOOLKIT, CMD5]
+    # TODO 最后一旦找到直接退出　md5为用户命令行输入的值，并将其全部转换为小写  加上随机数　每次从随机的一个位置开始查询防止被网站ban ip
+    ALLCRACK = [HASHCRACK, HASHTOOLKIT, CMD5, DMD5]
     global MD5
     MD5 = '202cb962ac59075b964b07152d234b70'
+    random_int = randint(0, len(ALLCRACK))
     for i in range(len(ALLCRACK)):
-        cracker = ALLCRACK[i](MD5)
+        cracker = ALLCRACK[(i + random_int) % len(ALLCRACK)](MD5)
         print('%s is cracking.' % cracker.url, end='')
         try:
             answer = cracker.crack()
